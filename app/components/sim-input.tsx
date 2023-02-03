c392a483ef8fa3af454b5363ffae0b1c068bdc99
effervescent000 <kislio01@gmail.com>
2023-02-02 21:28:25 -0500

    fix up sim input

--- app/components/sim-input.tsx
+++ app/components/sim-input.tsx
@@ -1,14 +1,35 @@
-import { AGES } from "~/constants/sim-data";
+import _ from "lodash";
 import Select from "./common/select";
 
-function SimInput() {
+import {
+  AGES,
+  TODDLER_TRAITS,
+  traitsKeyValuePairs,
+} from "~/constants/sim-data";
+
+function SimInput({ simId, label }: { simId: number; label?: string }) {
   return (
-    <>
+    <div>
+      {label && <span>{label}</span>}
       <Select
         label="Age"
         options={AGES.map((age, index) => ({ name: age, value: `${index}` }))}
+        fieldName={`sim-${simId}.age`}
       />
-    </>
+      <Select
+        label="Toddler trait"
+        options={TODDLER_TRAITS.map(({ key }) => ({ name: key, value: key }))}
+        fieldName={`sim-${simId}.toddlerTrait`}
+      />
+      {_.range(3).map((_n, i) => (
+        <Select
+          label="Trait"
+          options={traitsKeyValuePairs}
+          key={i}
+          fieldName={`sim-${simId}.trait-${i}`}
+        />
+      ))}
+    </div>
   );
 }
 
