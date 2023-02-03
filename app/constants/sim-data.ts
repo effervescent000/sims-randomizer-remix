f21cebc1022990b4b2f398172473e8fdeac8cba5
effervescent000 <kislio01@gmail.com>
2023-02-03 12:47:42 -0500

    complete overhaul of how traits are structured

--- app/constants/sim-data.ts
+++ app/constants/sim-data.ts
@@ -1,4 +1,11 @@
-import type { GenericObject, ISelectOptions, ITrait } from "~/types/interfaces";
+import {
+  EAges,
+  EPacks,
+  ETraits,
+  GenericObject,
+  ISelectOptions,
+  ITrait,
+} from "~/types/interfaces";
 import { humanizeKey } from "~/utils";
 
 export const AGES = ["Baby", "Toddler", "Child", "Teen", "YA+"];
@@ -14,95 +21,197 @@ export const TODDLER_TRAITS: GenericObject[] = [
   { key: "wild" },
 ];
 
-export const TRAITS: ITrait = {
-  adventurous: {
-    requires: ["snowyEscape"],
+export const TRAITS: { [key: number]: ITrait } = {
+  [ETraits.Adventurous]: {
+    requires: [EPacks.SnowyEscape],
   },
-  ambitious: {},
-  animalEnthusiast: {},
-  artLover: {},
-  bookworm: {},
-  bro: {},
-  catLover: {
-    requires: ["pets"],
+  [ETraits.Active]: { conflicts: [ETraits.Lazy] },
+  [ETraits.Ambitious]: {
+    conflicts: [ETraits.Lazy, ETraits.Freegan],
+    minAge: EAges.Adult,
   },
-  cheerful: {},
-  childOfTheIslands: {
-    requires: ["islandLiving"],
+  [ETraits.AnimalEnthusiast]: { requires: [EPacks.CottageLiving] },
+  [ETraits.ArtLover]: {},
+  [ETraits.Bookworm]: {},
+  [ETraits.Bro]: { minAge: EAges.Teen },
+  [ETraits.CatLover]: {
+    requires: [EPacks.Pets],
   },
-  childOfTheOcean: {
-    requires: ["islandLiving"],
+  [ETraits.Cheerful]: { conflicts: [ETraits.Gloomy, ETraits.HotHeaded] },
+  [ETraits.ChildOfTheIslands]: {
+    requires: [EPacks.IslandLiving],
   },
-  childish: {},
-  clumsy: {},
-  creative: {},
-  danceMachine: {
-    requires: ["getTogether"],
+  [ETraits.ChildOfTheOcean]: {
+    requires: [EPacks.IslandLiving],
   },
-  dogLover: {
-    conflicts: ["pets"],
+  [ETraits.Childish]: {
+    conflicts: [ETraits.Snob, ETraits.Evil, ETraits.HatesChildren],
+    minAge: EAges.Teen,
   },
-  erratic: {},
-  evil: {},
-  familyOriented: {},
-  foodie: {},
-  freegan: {
-    requires: ["ecoLifestyle"],
+  [ETraits.Clumsy]: {
+    minAge: EAges.Teen,
+    conflicts: [ETraits.Maker],
   },
-  geek: {},
-  genius: {},
-  gloomy: {},
-  glutton: {},
-  good: {},
-  goofball: {},
-  greenFiend: {
-    requires: ["ecoLifestyle"],
+  [ETraits.Creative]: {},
+  [ETraits.DanceMachine]: {
+    requires: [EPacks.GetTogether],
   },
-  hatesChildren: {},
-  highMaintenance: {
-    requires: ["spaDay"],
+  [ETraits.DogLover]: {
+    requires: [EPacks.Pets],
   },
-  hotHeaded: {},
-  insider: {
-    requires: ["getTogether"],
+  [ETraits.Erratic]: {},
+  [ETraits.Evil]: { conflicts: [ETraits.Good, ETraits.Childish] },
+  [ETraits.FamilyOriented]: {
+    conflicts: [ETraits.HatesChildren, ETraits.Noncommittal],
+    minAge: EAges.Adult,
   },
-  jealous: {},
-  kleptomaniac: {},
-  lactoseIntolerant: {
-    requires: ["cottageLiving"],
+  [ETraits.Foodie]: {
+    conflicts: [ETraits.Freegan, ETraits.Glutton],
+    minAge: EAges.Teen,
   },
-  lazy: {},
-  loner: {},
-  lovesOutdoors: {},
-  maker: {
-    requires: ["ecoLifestyle"],
+  [ETraits.Freegan]: {
+    requires: [EPacks.EcoLifestyle],
+    conflicts: [
+      ETraits.Ambitious,
+      ETraits.Foodie,
+      ETraits.Materialistic,
+      ETraits.Snob,
+      ETraits.Squeamish,
+    ],
+    minAge: EAges.Teen,
   },
-  materialistic: {},
-  mean: {},
-  musicLover: {},
-  neat: {},
-  noncommittal: {},
-  outgoing: {},
-  paranoid: {
-    requires: ["strangerVille"],
+  [ETraits.Geek]: {},
+  [ETraits.Genius]: {},
+  [ETraits.Gloomy]: {
+    conflicts: [ETraits.Cheerful, ETraits.HotHeaded, ETraits.PartyAnimal],
   },
-  perfectionist: {},
-  proper: {
-    conflicts: ["snowyEscape"],
+  [ETraits.Glutton]: { conflicts: [ETraits.Foodie, ETraits.Squeamish] },
+  [ETraits.Good]: {
+    conflicts: [ETraits.Evil, ETraits.Mean, ETraits.Kleptomaniac],
   },
-  recycleDisciple: {},
-  romantic: {},
-  selfAbsorbed: {
-    requires: ["getFamous"],
+  [ETraits.Goofball]: { conflicts: [ETraits.Snob] },
+  [ETraits.GreenFiend]: {
+    requires: [EPacks.EcoLifestyle],
   },
-  selfAssured: {},
-  slob: {},
-  snob: {},
-  squeamish: {
-    requires: ["outdoorRetreat"],
+  [ETraits.HatesChildren]: {
+    conflicts: [ETraits.FamilyOriented, ETraits.Childish],
+    minAge: EAges.Teen,
   },
-  unflirty: {},
-  vegetarian: {},
+  [ETraits.HighMaintenance]: {
+    requires: [EPacks.SpaDay],
+    minAge: EAges.Teen,
+  },
+  [ETraits.HotHeaded]: {
+    conflicts: [ETraits.Cheerful, ETraits.Gloomy],
+  },
+  [ETraits.Insider]: {
+    requires: [EPacks.GetTogether],
+    conflicts: [ETraits.Loner],
+  },
+  [ETraits.Jealous]: {
+    minAge: EAges.Teen,
+  },
+  [ETraits.Kleptomaniac]: { conflicts: [ETraits.Good] },
+  [ETraits.LactoseIntolerant]: {
+    requires: [EPacks.CottageLiving],
+  },
+  [ETraits.Lazy]: {
+    conflicts: [
+      ETraits.Active,
+      ETraits.Adventurous,
+      ETraits.Ambitious,
+      ETraits.Maker,
+      ETraits.Neat,
+      ETraits.Overachiever,
+    ],
+  },
+  [ETraits.Loner]: {
+    conflicts: [ETraits.Insider, ETraits.Outgoing, ETraits.PartyAnimal],
+  },
+  [ETraits.LovesOutdoors]: {
+    conflicts: [ETraits.Squeamish],
+  },
+  [ETraits.Maker]: {
+    requires: [EPacks.EcoLifestyle],
+    conflicts: [ETraits.Clumsy, ETraits.Lazy],
+    minAge: EAges.Teen,
+  },
+  [ETraits.Materialistic]: {
+    minAge: EAges.Teen,
+    conflicts: [ETraits.Freegan],
+  },
+  [ETraits.Mean]: {
+    conflicts: [ETraits.Good, ETraits.Proper],
+  },
+  [ETraits.MusicLover]: {},
+  [ETraits.Neat]: {
+    conflicts: [ETraits.Lazy, ETraits.Slob],
+  },
+  [ETraits.Noncommittal]: {
+    conflicts: [ETraits.FamilyOriented, ETraits.Loyal],
+    minAge: EAges.Adult,
+  },
+  [ETraits.Outgoing]: {
+    conflicts: [ETraits.Loner, ETraits.Paranoid, ETraits.SociallyAwkward],
+  },
+  [ETraits.Overachiever]: {
+    requires: [EPacks.HighSchoolYears],
+    conflicts: [ETraits.Lazy],
+    minAge: EAges.Teen,
+  },
+  [ETraits.Paranoid]: {
+    requires: [EPacks.StrangerVille],
+    conflicts: [ETraits.Outgoing],
+    minAge: EAges.Teen,
+  },
+  [ETraits.PartyAnimal]: {
+    requires: [EPacks.HighSchoolYears],
+    conflicts: [ETraits.Gloomy, ETraits.Loner, ETraits.SociallyAwkward],
+    minAge: EAges.Teen,
+  },
+  [ETraits.Perfectionist]: {},
+  [ETraits.Proper]: {
+    requires: [EPacks.SnowyEscape],
+    conflicts: [ETraits.Mean, ETraits.Slob],
+  },
+  [ETraits.RecycleDisciple]: {
+    requires: [EPacks.EcoLifestyle],
+  },
+  [ETraits.Romantic]: {
+    conflicts: [ETraits.Unflirty],
+    minAge: EAges.Teen,
+  },
+  [ETraits.SelfAbsorbed]: {
+    requires: [EPacks.GetFamous],
+  },
+  [ETraits.SelfAssured]: {},
+  [ETraits.Slob]: {
+    conflicts: [ETraits.Neat, ETraits.Proper, ETraits.Squeamish],
+  },
+  [ETraits.Snob]: {
+    conflicts: [ETraits.Childish, ETraits.Freegan, ETraits.Goofball],
+    minAge: EAges.Teen,
+  },
+  [ETraits.SociallyAwkward]: {
+    requires: [EPacks.HighSchoolYears],
+    conflicts: [ETraits.Outgoing, ETraits.PartyAnimal],
+    minAge: EAges.Teen,
+  },
+  [ETraits.Squeamish]: {
+    requires: [EPacks.OutdoorRetreat],
+    conflicts: [
+      ETraits.Glutton,
+      ETraits.LovesOutdoors,
+      ETraits.Slob,
+      ETraits.Freegan,
+    ],
+  },
+  [ETraits.Unflirty]: {
+    requires: [EPacks.CityLiving],
+    conflicts: [ETraits.Romantic],
+    minAge: EAges.Teen,
+  },
+  [ETraits.Vegetarian]: {},
 };
 
 export const traitsKeyValuePairs = Object.keys(TRAITS).reduce(
--- app/types/interfaces.ts
+++ app/types/interfaces.ts
@@ -3,10 +3,109 @@ export interface GenericObject {
 }
 
 export interface ITrait {
-  [key: string]: { conflicts?: string[]; requires?: string[] };
+  conflicts?: ETraits[];
+  requires?: EPacks[];
+  minAge?: EAges;
 }
 
 export interface ISelectOptions {
   name: string;
   value: string;
+}
+
+export interface ISim {
+  age: number;
+  toddlerTrait?: string;
+  traits?: string[];
+  aspiration?: string;
+  career?: string;
+}
+
+// ENUMS
+
+export enum EPacks {
+  CityLiving,
+  CottageLiving,
+  EcoLifestyle,
+  GetFamous,
+  GetTogether,
+  HighSchoolYears,
+  IslandLiving,
+  OutdoorRetreat,
+  Pets,
+  SnowyEscape,
+  SpaDay,
+  StrangerVille,
+}
+
+export enum EAges {
+  Baby,
+  Toddler,
+  Child,
+  Teen,
+  Adult,
+}
+
+export enum ETraits {
+  Adventurous,
+  Active,
+  Ambitious,
+  AnimalEnthusiast,
+  ArtLover,
+  Bookworm,
+  Bro,
+  CatLover,
+  Cheerful,
+  ChildOfTheIslands,
+  ChildOfTheOcean,
+  Childish,
+  Clumsy,
+  Creative,
+  DanceMachine,
+  DogLover,
+  Erratic,
+  Evil,
+  FamilyOriented,
+  Foodie,
+  Freegan,
+  Geek,
+  Genius,
+  Gloomy,
+  Glutton,
+  Good,
+  Goofball,
+  GreenFiend,
+  HatesChildren,
+  HighMaintenance,
+  HotHeaded,
+  Insider,
+  Jealous,
+  Kleptomaniac,
+  LactoseIntolerant,
+  Lazy,
+  Loner,
+  LovesOutdoors,
+  Loyal,
+  Maker,
+  Materialistic,
+  Mean,
+  MusicLover,
+  Neat,
+  Noncommittal,
+  Overachiever,
+  Outgoing,
+  Paranoid,
+  PartyAnimal,
+  Perfectionist,
+  Proper,
+  RecycleDisciple,
+  Romantic,
+  SelfAbsorbed,
+  SelfAssured,
+  Slob,
+  Snob,
+  SociallyAwkward,
+  Squeamish,
+  Vegetarian,
+  Unflirty,
 }
